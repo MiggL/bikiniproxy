1074297011359eb46bb71d6d7ce9ed9fc86b9481
miguel <miguelmu@kth.se>
2021-05-26 16:50:58 +0200

    Separate pipeline from data aggregation

--- DeadClick/script/fetchPbInducedErrors.js
+++ DeadClick/script/fetchPbInducedErrors.js
@@ -13,33 +13,6 @@ function getState(url, proxyUrl = null, extension = null) {
   });
 }
 
-function deleteUrlWebtraces(url, webtracesDir) {
-  const directory = path.join(webtracesDir, md5(url));
-  fs.readdir(directory, (err, files) => {
-    if (err) throw err;
-    files.forEach(file => file === 'request.json' || file === 'screenshot.png' || fs.unlinkSync(path.join(directory, file)));
-    // fs.rmdirSync(directory);
-  });
-}
-
-function addToJsonArrayFile(jsonObj, filepath) {
-  const dir = filepath.slice(0, filepath.lastIndexOf('/'));
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-  }
-  fs.readFile(filepath, (err, data) => {
-    try {
-      const contentArray = err ? [] : JSON.parse(data); // err should only occur if file doesn't exist
-      if (!contentArray.map(JSON.stringify).includes(JSON.stringify(jsonObj))) {
-        contentArray.push(jsonObj);
-        fs.writeFileSync(filepath, JSON.stringify(contentArray, null, 2));
-      }
-    } catch (e) {
-      addToJsonArrayFile(jsonObj, filepath);
-    }
-  });
-}
-
 function extractAddedErrors(expectedErrors, errors) {
   const addedErrors = [];
   for (const error of errors) {
@@ -64,36 +37,38 @@ async function collectUrls(urls, webtracesDir = utils.requestsPath) {
     try {
       const collectedState = await getState(url);
       await stateCollector.serializeState(collectedState, webtracesDir);
-      addToJsonArrayFile(url, path.join(fromWebDataDir, 'collected.json'));
     } catch (e) {
       console.error(e);
-      await utils.closeBrowser();
-      addToJsonArrayFile(url, path.join(fromWebDataDir, 'notCollected.json'));
       continue;
+    } finally {
+      await utils.closeBrowser();
     }
     try {
       console.log('reproducing');
-      const expectedState = await utils.loadState(url, webtracesDir); // maybe fetch from proxy for consistency?
-      const isReproduced = await reproductionProxy.reproduceRequestState(expectedState, proxy);
-      console.log(isReproduced.identical);
-      if (isReproduced.identical === true) {
-        addToJsonArrayFile(url, path.join(fromWebDataDir, 'reproduced.json'));
-      } else {
-        addToJsonArrayFile(url, path.join(fromWebDataDir, 'notReproduced.json'));
-        deleteUrlWebtraces(url, webtracesDir);
-        continue;
+      const expectedState = await utils.loadState(url, webtracesDir);
+      const { identical, type, actual } = await reproductionProxy.reproduceRequestState(expectedState, proxy);
+      for (let j = 0; j < actual.requests.length; j++) {
+        delete actual.requests[j].body;
       }
+      console.log(identical);
+      expectedState.reproduced = identical ? { identical } : { identical, type, actual };
+      fs.writeFileSync(path.join(webtracesDir, md5(url), 'request.json'), JSON.stringify(expectedState));
     } catch (e) {
       console.error(e);
+    } finally {
       await utils.closeBrowser();
-      addToJsonArrayFile(url, path.join(fromWebDataDir, 'errorReproducing.json'));
-      deleteUrlWebtraces(url, webtracesDir);
-      continue;
     }
   }
 }
 
 async function trainPb(urls, webtracesDir = utils.requestsPath, proxy) {
+  // If needed, uncomment and use time to manually import training data.
+  /*const expectedState = await utils.loadState(urls[0], webtracesDir);
+  proxy.requestState = expectedState;
+  await getState(urls[0], 'localhost:' + proxy.port, path.join(__dirname, '../../privacy-badger/2020.8.25_0'));
+  proxy.requestState = await utils.loadState(urls[4269], webtracesDir);
+  await getState(urls[0], 'localhost:' + proxy.port, path.join(__dirname, '../../privacy-badger/2020.8.25_0'));
+  */
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i];
     console.log('\nVisiting ' + (i + 1) + '/' + urls.length);
@@ -116,6 +91,7 @@ async function extractPbErrors(urls, webtracesDir = utils.requestsPath, webtrace
   }
   const proxy = await reproductionProxy.startReproductionProxy(8002, webtracesDir);
 
+  // No need to train new versions of Privacy Badger.
   await trainPb(urls, webtracesDir, proxy);
 
   for (let i = 0; i < urls.length; i++) {
@@ -126,32 +102,23 @@ async function extractPbErrors(urls, webtracesDir = utils.requestsPath, webtrace
     try {
       proxy.requestState = expectedState;
       const collectedPbState = await getState(url, 'localhost:' + proxy.port, path.join(__dirname, '../../privacy-badger/2020.8.25_0'));
-      console.log(collectedPbState.errors.map(e => e.exceptionDetails.exception.description));
+      //console.log(collectedPbState.errors.map(e => e.exceptionDetails.exception.description));
       await stateCollector.serializeState(collectedPbState, webtracesPbDir);
-      addToJsonArrayFile(url, path.join(fromProxyDataDir, 'pbCollected.json'));
     } catch (e) {
       console.error(e);
-      addToJsonArrayFile(url, path.join(fromProxyDataDir, 'notPbCollected.json'));
-      deleteUrlWebtraces(url, webtracesDir);
       continue;
     }
-    const pbState = await utils.loadState(url, webtracesPbDir);
-    const pbErrors = extractAddedErrors(expectedState.errors, pbState.errors);
-    if (pbErrors.length === 0) {
-      deleteUrlWebtraces(url, webtracesDir);
-      deleteUrlWebtraces(url, webtracesPbDir);
-    }
-    addToJsonArrayFile({ url: url, addedPbErrors: pbErrors }, path.join(fromProxyDataDir, 'addedPbErrors.json'));
   }
   proxy.close();
 }
 
 (async () => {
   const webtracesDir = path.join(__dirname, '../data_sep_2020/randomDuckduckgoUrls/webtraces/');
-  const webtracesPbDir = path.join(webtracesDir, '../webtraces_pb_repaired_new/');
+  const webtracesPbDir = path.join(webtracesDir, '../webtraces_pb/');
+  const urls = JSON.parse(fs.readFileSync(path.join(__dirname, 'collectedDuckduckgoUrls.json')));
   await collectUrls(urls, webtracesDir);
   await utils.closeBrowser();
   await extractPbErrors(urls, webtracesDir, webtracesPbDir);
-  await utils.closeBrowser();
-  process.exit(0);
+  //await utils.closeBrowser();
+  //process.exit(0);
 })();
